a8d6343817c2ac070d0d0ad8d404717c29d8ae39
AmiraAHady <amirasgithub@gmail.com>
2023-03-11 12:05:59 +0200

    adding movie details layout

--- src/app/movies-details/movies-details.component.ts
+++ src/app/movies-details/movies-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesDataService } from '../movies-data.service';
+import { Imovie } from '../movies/Imovie';
 
 @Component({
   selector: 'app-movies-details',
@@ -8,13 +9,13 @@ import { MoviesDataService } from '../movies-data.service';
   styleUrls: ['./movies-details.component.css']
 })
 export class MoviesDetailsComponent implements OnInit {
-   singleMovie={}
+   singleMovie!: Imovie;
   constructor(private moviesServ:MoviesDataService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     let id=this.route.snapshot.paramMap.get('id');
     this.moviesServ.getMovieById(id!).subscribe({next:(movie)=>{
-      this.singleMovie=movie;
+      // this.singleMovie=movie;
       console.log(movie);
       
     }})
